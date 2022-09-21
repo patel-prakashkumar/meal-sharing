e20aee1ad617a785f36514a8376c8de5a0fdd95d
Prakashkumar Patel <patelprakash0508@gmail.com>
2022-09-21 20:49:35 +0200

    change branch

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -1,15 +1,27 @@
+const { query } = require("express");
 const express = require("express");
 const router = express.Router();
 const knex = require("../database");
-
-router.get("/", async (request, response) => {
+/* router.get("/", async (req, res) => {
   try {
     // knex syntax for selecting things. Look up the documentation for knex for further info
-    const titles = await knex("meals").select("title");
-    response.json(titles);
+    const mealTable = await knex.select().table('meal');
+    res.json(mealTable);
   } catch (error) {
-    throw error;
+    throw res.status(404).json(error);
+  }
+}); */
+router.get("/", async (req, res) => {
+  try {
+    // knex syntax for selecting things. Look up the documentation for knex for further info
+    const maxPrice = req.query;
+    const mealTable = await knex('meal').max(maxPrice);
+    res.json(mealTable);
+  } catch (error) {
+    throw res.status(404).json(error);
   }
 });
 
+
 module.exports = router;
+
