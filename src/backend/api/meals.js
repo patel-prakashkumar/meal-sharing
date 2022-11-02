501aed8d68f2f47549e92eaf2ea1e6d5f28e48c4
Prakashkumar Patel <patelprakash0508@gmail.com>
2022-11-02 12:58:00 +0100

    resolve conflict error

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -1,12 +1,12 @@
+// @ts-ignore
 const { query } = require("express");
 const express = require("express");
+// @ts-ignore
 const { max } = require("../database");
 const router = express.Router();
 const knex = require("../database");
 
 router.get("/", async (req, res) => {
-<<<<<<< HEAD
-
   if ("maxPrice" in req.query) {
     const maxPrice = Number(req.query.maxPrice);
     if (!maxPrice) {
@@ -47,6 +47,7 @@ router.get("/", async (req, res) => {
   }
   if ("dateAfter" in req.query) {
     const dateAfter = req.query.dateAfter;
+    // @ts-ignore
     const date = new Date(dateAfter);
     console.log(date)
     if (!date) {
@@ -62,6 +63,7 @@ router.get("/", async (req, res) => {
   }
   if ("dateBefore" in req.query) {
     const dateBefore = req.query.dateAfter;
+    // @ts-ignore
     const date = new Date(dateBefore);
     if (!date) {
       res.status(403).json('Please Enter Date')
@@ -146,8 +148,8 @@ router.get("/", async (req, res) => {
         res.status(403).json("Enter valid Sort_key")
       }
     }
-=======
-  try {
+  }
+ try {
     // knex syntax for selecting things. Look up the documentation for knex for further info
     const findTableData = await knex.select().table('meal')
     if (findTableData.length === 0) {
@@ -157,54 +159,53 @@ router.get("/", async (req, res) => {
     }
   } catch (error) {
     res.status(404).json({ error: "Bad Get Request" });
-  }
+  } 
 });
-// Add Endpoint with req.body 
-router.post("/", async (req, res) => {
-  try {
-    const insertData = await knex('meal').insert({ title: req.body.title, description: req.body.description, location: req.body.location, when: req.body.when, max_reservations: req.body.max_reservations, price: req.body.price, created_date: req.body.created_date});
-    res.json(insertData);
-  } catch (error) {
-    res.status(403).json({ error: "Failed to Insert data in Table" });
-  }
-})
-
-router.get("/:id", async (req, res) => {
-  try {
-    // knex syntax for selecting things. Look up the documentation for knex for further info
-    const data = await knex.select().table('meal').where('id', req.params.id);
-    if (data.length === 0) {
-      res.status(404).json("Id is not available in database")
-    } else {
-      res.json(data);
+  // Add Endpoint with req.body 
+  router.post("/", async (req, res) => {
+    try {
+      const insertData = await knex('meal').insert({ title: req.body.title, description: req.body.description, location: req.body.location, when: req.body.when, max_reservations: req.body.max_reservations, price: req.body.price, created_date: req.body.created_date });
+      res.json(insertData);
+    } catch (error) {
+      res.status(403).json({ error: "Failed to Insert data in Table" });
     }
-  } catch (error) {
-    res.status(404).json({ error: "Bad Request" });
->>>>>>> 144d4f67a4b5b65f4b66c3fbdb9d0818e175fe9c
-  }
-});
+  })
 
-router.put("/:id", async (req, res) => {
-  try {
-    const updateData = await knex('meal').where('id', req.params.id).update('title', 'Lunch')
-    res.json(updateData)
-  } catch (error) {
-    res.status(400).json({ error: "unsuccessful PUT request" });
-  }
-})
-
-router.delete("/:id", async (req, res) => {
-  try {
-    const deleteData = await knex('meal').where('id', req.params.id).del();
-   if (!deleteData) {
-      res.status(404).json({ error: "Id doesn't exist in table" })
-    } else {
-      res.json({ "message": "Deleted meal" , Key: deleteData})
+  router.get("/:id", async (req, res) => {
+    try {
+      // knex syntax for selecting things. Look up the documentation for knex for further info
+      const data = await knex.select().table('meal').where('id', req.params.id);
+      if (data.length === 0) {
+        res.status(404).json("Id is not available in database")
+      } else {
+        res.json(data);
+      }
+    } catch (error) {
+      res.status(404).json({ error: "Bad Request" });
     }
-  } catch (error) {
-    res.status(409).json({ error: "unsuccessful Delete request" });
-  }
-})
+  });
 
-module.exports = router;
+  router.put("/:id", async (req, res) => {
+    try {
+      const updateData = await knex('meal').where('id', req.params.id).update('title', 'Lunch')
+      res.json(updateData)
+    } catch (error) {
+      res.status(400).json({ error: "unsuccessful PUT request" });
+    }
+  })
+
+  router.delete("/:id", async (req, res) => {
+    try {
+      const deleteData = await knex('meal').where('id', req.params.id).del();
+      if (!deleteData) {
+        res.status(404).json({ error: "Id doesn't exist in table" })
+      } else {
+        res.json({ "message": "Deleted meal", Key: deleteData })
+      }
+    } catch (error) {
+      res.status(409).json({ error: "unsuccessful Delete request" });
+    }
+  })
+
+  module.exports = router;
 
